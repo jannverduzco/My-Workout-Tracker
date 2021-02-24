1fe48285441ed71231505e8237071eb310575f89
Janneth Verduzco <janneth_verduzco@live.com>
2021-02-24 13:58:05 -0600

    set up serve.js with code

---
+++ server.js
@@ -0,0 +1,26 @@
+// DEPENDENCIES
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Port
+const PORT = process.env.PORT || 3000;
+
+// Setting up Express App and middleware
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Setting up mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// Settign up Port
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`);
+  });
+  
