64db833c1cd3147ebae77fea7a144342d1e30a59
Janneth Verduzco <janneth_verduzco@live.com>
2021-02-25 17:12:16 -0600

    added the models and routes folder with their corresponding files. Added dependecies (code) to each file

---
+++ models/workouts.js
@@ -0,0 +1,3 @@
+// Requeing mongoose and the shcema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
---
+++ routes/api.js
@@ -0,0 +1,3 @@
+// Requiring dependecies (express and the file in our models folder)
+const router = require("express").Router();
+const workouts = require("../models/workouts.js");
