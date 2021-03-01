ba4e4c59e68dc96d92d4853319ed2e34d5e5d8a6
Janneth Verduzco <janneth_verduzco@live.com>
2021-02-28 22:31:23 -0600

    updated server.js dependencise and added pseudo code to apiROutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,17 +2,20 @@
 const workouts = require("../models/workout.js");
 
 module.exports = (app) => {
+  // GET route to get all workouts
   app.get("/api/workouts", (req, res) => {
     workouts
       .find({})
       .then((data) => {
         res.json(data);
+        cd;
       })
       .catch((err) => {
         res.json(err);
       });
   });
 
+  // PUT workout to update a specific workout
   app.put("/api/workouts/:id", (req, res) => {
     workouts
       .findByIdAndUpdate(
@@ -30,17 +33,29 @@ module.exports = (app) => {
       });
   });
 
-  app.post("/api/workouts", (req, res) => {});
+  // POST workout to add workout
+  app.post("/api/workouts", (req, res) => {
+    workouts
+      .create(req.body)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 
+  // POST workout to add workout range
   app.get("/api/workouts/range", (req, res) => {
     workouts
-    // grabbing the last 7 docs in collection
-    .find({}).limit(7)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+      // grabbing the last 7 docs in collection
+      .find({})
+      .limit(7)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
   });
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 // Port
 const PORT = process.env.PORT || 3000;
@@ -8,20 +9,21 @@ const PORT = process.env.PORT || 3000;
 // Setting up Express App and middleware
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-// Passing instance of espress through routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
 
 // Setting up mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useFindAndModify: false
 });
+
+// Passing instance of espress through routes
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
 // Settign up Port
 app.listen(PORT, () => {
