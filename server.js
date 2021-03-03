394709c44ad8d68030a64623fc6af2b3a5a1be21
Janneth Verduzco <janneth_verduzco@live.com>
2021-03-02 22:58:12 -0600

    made changes to server.js due to app working locally but not heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 // Setting up Express App and middleware
 const app = express();
@@ -12,9 +13,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use(logger("dev"));
+
 // Setting up mongoose
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workouttracker',
+    process.env.MONGODB_URI || "mongodb://localhost/workouttracker",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
