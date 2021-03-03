78f18d2d9e9e4a04f9528776d855727b90f19cb2
Janneth Verduzco <janneth_verduzco@live.com>
2021-03-02 22:00:43 -0600

    added ALTAS mongoosDB

--- server.js
+++ server.js
@@ -12,12 +12,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
 // Setting up mongoose
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouttracker", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workouttracker',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 // Passing instance of espress through routes
 require("./routes/apiRoutes.js")(app);
@@ -25,6 +29,5 @@ require("./routes/htmlRoutes.js")(app);
 
 // Settign up Port
 app.listen(PORT, () => {
-    console.log(`App running on http://localhost:${PORT}`);
-  });
-  
+  console.log(`App running on http://localhost:${PORT}`);
+});
